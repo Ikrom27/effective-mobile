f269e57c12b78dcc54063d7cad50e104613072d0
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-07-04 14:51:29 +0300

    init fragments

---
+++ app/src/main/java/com/ikrom/effective_mobile/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.ikrom.effective_mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.ikrom.effective_mobile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupNavigation()
+    }
+
+    private fun setupNavigation(){
+        val navController: NavController = findNavController(R.id.navHostContainer)
+        NavigationUI.setupWithNavController(binding.bottomNavBar, navController)
+    }
+}
