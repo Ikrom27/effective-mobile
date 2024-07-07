100a0492a4b0cb4823fa6ede62c34014bc8f6da0
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-07-07 18:34:49 +0300

    subscriptions module created

--- feature/subscriptions/build.gradle.kts
+++ feature/subscriptions/build.gradle.kts
@@ -1,25 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    kotlin("kapt")
 }
 
 android {
-    namespace = "com.ikrom.effective_mobile"
+    namespace = "com.ikrom.subscribtions"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ikrom.effective_mobile"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildFeatures {
-        viewBinding = true
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -41,21 +33,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.dagger)
-    kapt(libs.dagger.compiler)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(project(":feature:hotels"))
-    implementation(project(":data"))
-    implementation(project(":core-ui"))
-    implementation(project(":feature:tickets"))
-    implementation(project(":feature:shorter"))
-    implementation(project(":feature:subscriptions"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
