6b6788a8734bac79f654a0daf073a29a18798092
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-07-07 18:29:09 +0300

    shorter module created

--- feature/shorter/src/main/java/com/ikrom/shorter/ShorterFragment.kt
+++ feature/shorter/src/main/java/com/ikrom/shorter/ShorterFragment.kt
@@ -1,11 +1,10 @@
-package com.ikrom.effective_mobile.screens
+package com.ikrom.shorter
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ikrom.effective_mobile.R
 
 class ShorterFragment : Fragment() {
 
